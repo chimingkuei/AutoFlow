b396524620891f21651584770bd1e08fd4d1d021
chimingkuei <chimingkuei@gmail.com>
2024-01-11 15:26:00 +0800

    AutoFlow

---
+++ AutoFlow/Core.cs
@@ -0,0 +1,303 @@
+﻿using AutoFlow;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Drawing.Chart;
+using OpenCvSharp.Extensions;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Markup;
+using System.Windows.Media.Imaging;
+using System.Windows.Interop;
+
+namespace AutoFlow
+{
+    class Core
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo);
+
+        // 定義滑鼠事件的標誌位
+        const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        const uint MOUSEEVENTF_LEFTUP = 0x0004;
+        const uint MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        const uint MOUSEEVENTF_RIGHTUP = 0x0010;
+
+        public IntPtr PackFindWindow(string lpClassName, string lpWindowName)
+        {
+            return FindWindow(lpClassName, lpWindowName);
+        }
+
+        public bool PackSetForegroundWindow(IntPtr hWnd)
+        {
+            return SetForegroundWindow(hWnd);
+        }
+
+        public void SimulateLeftMouseClick(IntPtr windowHandle, int x, int y)
+        {
+            SetCursorPos(x, y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, IntPtr.Zero);
+            Thread.Sleep(100);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, IntPtr.Zero);
+        }
+
+        public void SimulateRightMouseClick(IntPtr windowHandle, int x, int y)
+        {
+            SetCursorPos(x, y);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, IntPtr.Zero);
+            Thread.Sleep(100);
+            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, IntPtr.Zero);
+        }
+
+        #region Close Caps Lock
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        const int KEYEVENTF_EXTENDEDKEY = 0x1;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        public static extern short GetKeyState(int keyCode);
+
+        public void CloseCapsLock()
+        {
+            bool CapsLock = (((ushort)GetKeyState(0x14)) & 0xffff) != 0;
+            if (CapsLock)
+                keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+        }
+        #endregion
+
+        #region Load English Input Method
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr LoadKeyboardLayout(string pwszKLID, uint Flags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr ActivateKeyboardLayout(IntPtr hkl, uint Flags);
+
+        // 定義輸入法標識符號
+        const string ENGLISH_KEYBOARD_LAYOUT_ID = "00000409"; // 英文（美國）
+        // 定義激活輸入法標誌
+        const uint KLF_ACTIVATE = 1;
+
+        public void LoadEIM()
+        {
+            IntPtr englishLayout = LoadKeyboardLayout(ENGLISH_KEYBOARD_LAYOUT_ID, 0);
+            if (englishLayout == IntPtr.Zero)
+            {
+                Console.WriteLine("載入輸入法失敗!");
+                return;
+            }
+            IntPtr result = ActivateKeyboardLayout(englishLayout, KLF_ACTIVATE);
+            if (result == IntPtr.Zero)
+            {
+                Console.WriteLine("激活輸入法失敗!");
+                return;
+            }
+        }
+        #endregion
+
+        private BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new System.IO.MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+
+        public void CaptureScreen(System.Windows.Controls.Image display_image)
+        {
+            Rectangle screenBounds = Screen.PrimaryScreen.Bounds;
+            Bitmap screenshot = new Bitmap(screenBounds.Width, screenBounds.Height, PixelFormat.Format32bppArgb);
+
+            using (Graphics graphics = Graphics.FromImage(screenshot))
+            {
+                graphics.CopyFromScreen(screenBounds.X, screenBounds.Y, 0, 0, screenBounds.Size, CopyPixelOperation.SourceCopy);
+            }
+            BitmapImage bitmapImage = ConvertBitmapToBitmapImage(screenshot);
+            display_image.Source = bitmapImage;
+        }
+        
+
+    }
+
+    class ExcelHandler
+    {
+        public void CreateXlsx(string filepath, string sheetname)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            // 新建一個 Excel 檔案
+            var excelFile = new ExcelPackage();
+            // 在 Excel 檔案中建立一個工作表
+            var worksheet = excelFile.Workbook.Worksheets.Add(sheetname);
+            // 在工作表中新增一些資料
+            // example:
+            worksheet.Cells["A1"].Value = "姓名";
+            // 儲存 Excel 檔案
+            try
+            {
+                excelFile.SaveAs(new FileInfo(filepath));
+            }
+            catch
+            {
+                Console.WriteLine(filepath + " file is opened! Please close that file.");
+            }
+        }
+
+        public void ReadXlsx(string filepath)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            // 路徑到Excel文件
+            var fileInfo = new FileInfo(filepath);
+            // 使用ExcelPackage讀取Excel文件
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                // 取得第一個工作表
+                var worksheet = package.Workbook.Worksheets[0];
+                // 讀取單元格的值
+                // example:
+                //var value = worksheet.Cells[1, 1].Value;
+            }
+        }
+
+        public void ModifyXlsx(string filepath)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            // 路徑到Excel文件
+            var fileInfo = new FileInfo(filepath);
+            // 使用ExcelPackage讀取Excel文件
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                // 取得第一個工作表
+                var worksheet = package.Workbook.Worksheets[0];
+                // 修改單元格的值
+                // example:
+                //worksheet.Cells[1, 1].Value = "Hello, world!";
+                // 保存Excel文件
+                try
+                {
+                    package.Save();
+                }
+                catch
+                {
+                    Console.WriteLine(filepath + " file is opened! Please close that file.");
+                }
+            }
+        }
+
+        private List<Tuple<string, string, string, Color>> SetCategoryColor()
+        {
+            List<Tuple<string, string, string, Color>> categorycolor = new List<Tuple<string, string, string, Color>>
+            {
+                new Tuple<string, string, string, Color>("M1:M", "N1:N", "Label A", Color.Red),
+                new Tuple<string, string, string, Color>("O1:O", "P1:P", "Label B",Color.Green),
+                new Tuple<string, string, string, Color>("Q1:Q", "R1:R", "Label C",Color.Blue)
+            };
+            return categorycolor;
+        }
+
+        private void SetChartStyle(ExcelChart chart)
+        {
+            chart.SetPosition(1, 0, 4, 0); // Set chart position
+            chart.SetSize(600, 400); // Set chart size
+            chart.Title.Text = "圖表標題";// Set chart title
+            chart.Title.Fill.Color = Color.Cyan;// Set color of chart title
+            chart.Legend.Position = eLegendPosition.Right;// Set position of legend
+            chart.Legend.Fill.Color = Color.LightGray;// Set color of legend
+            chart.XAxis.Title.Text = "X Axis Title";
+            chart.XAxis.MajorGridlines.Fill.Color = Color.Gray;
+            chart.XAxis.MinorGridlines.Fill.Color = Color.LightGray;
+            chart.XAxis.MinValue = 0;
+            chart.XAxis.MaxValue = 20;
+            chart.YAxis.Title.Text = "Y Axis Title";
+            chart.YAxis.MajorGridlines.Fill.Color = Color.Gray;
+            chart.YAxis.MinorGridlines.Fill.Color = Color.LightGray;
+            chart.YAxis.MinValue = 0;
+            chart.YAxis.MaxValue = 20;
+        }
+
+        private bool CheckChartName(ExcelWorksheet worksheet, string chartname)
+        {
+            foreach (ExcelDrawing drawing in worksheet.Drawings)
+            {
+                if (drawing.Name == chartname)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // The name of chart can't be repeated.(ScatterPlot)
+        public void ScatterChart(string filepath, List<List<Tuple<double, double>>> lists)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var fileInfo = new FileInfo(filepath);
+            using (var package = new ExcelPackage(fileInfo))
+            {
+                var worksheet = package.Workbook.Worksheets[0];
+                if (!CheckChartName(worksheet, "ScatterPlot"))
+                {
+                    var chart = worksheet.Drawings.AddChart("ScatterPlot", eChartType.XYScatter);
+                    SetChartStyle(chart);
+                    // Set X and Y axis data ranges
+                    for (int list_index = 0; list_index < lists.Count; list_index++)
+                    {
+                        for (int i = 0; i < lists[list_index].Count; i++)
+                        {
+                            worksheet.Cells[SetCategoryColor()[list_index].Item1[0]+(i+1).ToString()].Value = lists[list_index][i].Item1;
+                            worksheet.Cells[SetCategoryColor()[list_index].Item2[0] + (i + 1).ToString()].Value = lists[list_index][i].Item2;
+                        }
+                        var xRange = worksheet.Cells[SetCategoryColor()[list_index].Item1 + lists[list_index].Count];
+                        var yRange = worksheet.Cells[SetCategoryColor()[list_index].Item2 + lists[list_index].Count];
+                        var series = (ExcelScatterChartSerie)chart.Series.Add(xRange, yRange);
+                        series.Header = SetCategoryColor()[list_index].Item3;
+                        series.Marker.Fill.Color = SetCategoryColor()[list_index].Item4;
+                        series.Marker.Style = eMarkerStyle.Circle;
+                    }
+                }
+                try
+                {
+                    package.Save();
+                }
+                catch
+                {
+                    Console.WriteLine(filepath + " file is opened! Please close that file.");
+                }
+
+            }
+        }
+
+       
+
+
+    }
+}
---
+++ AutoFlow/MainWindow.xaml.cs
@@ -0,0 +1,307 @@
+﻿using Newtonsoft.Json.Linq;
+using OfficeOpenXml.Drawing.Chart;
+using OfficeOpenXml;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static AutoFlow.BaseLogRecord;
+using Color = System.Drawing.Color;
+using OfficeOpenXml.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Interop;
+using System.Diagnostics;
+
+namespace AutoFlow
+{
+    public class Parameter
+    {
+        public string Window_Name_val { get; set; }
+        public string Coordinate_X_val { get; set; }
+        public string Coordinate_Y_val { get; set; }
+        public List<System.Drawing.Point> click_points { get; set; }
+    }
+    
+    public partial class MainWindow : System.Windows.Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("請問是否要關閉？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #region NotifyIcon
+        private System.Windows.Forms.NotifyIcon notifyIcon = null;
+        System.Windows.Forms.ContextMenu nIconMenu = new System.Windows.Forms.ContextMenu();
+        System.Windows.Forms.MenuItem nIconMenuItem1 = new System.Windows.Forms.MenuItem();
+        private void InitialTray()
+        {
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon(@"Icon/Deepwise.ico");
+            notifyIcon.Text = "Elf";
+            notifyIcon.Visible = true;
+            notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(notifyIcon_MouseClick);
+            this.StateChanged += new EventHandler(WPFUI_StateChanged);
+            //小圖示選單
+            nIconMenuItem1.Index = 0;
+            nIconMenuItem1.Text = "結束";
+            nIconMenuItem1.Click += new System.EventHandler(nIconMenuItem1_Click);
+            nIconMenu.MenuItems.Add(nIconMenuItem1);
+            notifyIcon.ContextMenu = nIconMenu;
+        }
+        private void notifyIcon_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            //如果鼠标左键单击
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                if (this.Visibility == Visibility.Visible)
+                {
+                    this.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    this.Show();
+                    this.WindowState = (WindowState)System.Windows.Forms.FormWindowState.Normal;
+                }
+
+            }
+        }
+        private void WPFUI_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void nIconMenuItem1_Click(object sender, System.EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        #endregion
+
+        public string TextBoxDispatcherGetValue(TextBox control)
+        {
+            string name = "";
+            this.Dispatcher.Invoke(() =>
+            {
+                name = control.Text;
+            });
+            return name;
+
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        #region Config
+        private void LoadConfig()
+        {
+            List<Parameter> Parameter_info = Config.Load();
+            Window_Name.Text = Parameter_info[0].Window_Name_val;
+            Coordinate_X.Text = Parameter_info[0].Coordinate_X_val;
+            Coordinate_Y.Text = Parameter_info[0].Coordinate_Y_val; 
+        }
+
+        private void SaveConfig()
+        {
+            List<Parameter> Parameter_config = new List<Parameter>()
+            {
+                new Parameter() {
+                    Window_Name_val = Window_Name.Text,
+                    Coordinate_X_val = Coordinate_X.Text,
+                    Coordinate_Y_val = Coordinate_Y.Text,
+                    click_points = pointlist
+                }
+            };
+            Config.Save(Parameter_config);
+
+        }
+        #endregion
+
+        private void GetClickPoint()
+        {
+            // Image控件顯示紅點
+            Ellipse reddot = new Ellipse
+            {
+                Stroke = System.Windows.Media.Brushes.Red,
+                StrokeThickness = 5
+            };
+            Canvas.SetLeft(reddot, _downPoint.X);
+            Canvas.SetTop(reddot, _downPoint.Y);
+            reddotlist.Add(reddot);
+            canvas.Children.Add(reddot);
+            // List螢幕座標位置
+            int screen_x = Convert.ToInt32(_downPoint.X / Display_Image.ActualWidth * 1920);
+            int screen_y = Convert.ToInt32(_downPoint.Y / Display_Image.ActualHeight * 1080);
+            //Console.WriteLine($"螢幕X座標:{screen_x}");
+            //Console.WriteLine($"螢幕Y座標:{screen_y}");
+            pointlist.Add(new System.Drawing.Point(screen_x, screen_y));
+        }
+
+        private void RemoveClickPoint()
+        {
+            if (reddotlist.Count != 0)
+            {
+                canvas.Children.Remove(reddotlist[reddotlist.Count - 1]);
+                reddotlist.Remove(reddotlist[reddotlist.Count - 1]);
+                pointlist.Remove(pointlist[pointlist.Count - 1]);
+            }
+        }
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitialTray();
+            LoadConfig();
+            //Do.LoadEIM();
+            //Do.CloseCapsLock();
+        }
+        BaseConfig<Parameter> Config = new BaseConfig<Parameter>();
+        Core Do = new Core();
+        BaseLogRecord Logger = new BaseLogRecord();
+        CancellationTokenSource cts;
+        private bool _started;
+        private System.Windows.Point _downPoint;
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(Start):
+                    {
+                        cts = new CancellationTokenSource();
+                        Task.Run(() =>
+                        {
+                            while (true)
+                            {
+                                if (cts.Token.IsCancellationRequested)
+                                {
+                                    Console.WriteLine("Stop running the software.");
+                                    return;
+                                }
+                                IntPtr targetWindowHandle = Do.PackFindWindow(null, TextBoxDispatcherGetValue(Window_Name));
+                                if (targetWindowHandle != IntPtr.Zero)
+                                {
+                                    #region Get window position and size.
+                                    //Console.WriteLine($"找到了 {TextBoxDispatcherGetValue(Window_Name)} 的視窗句柄: {targetWindowHandle}");
+                                    //RECT windowRect;
+                                    //GetWindowRect(targetWindowHandle, out windowRect);
+                                    //Console.WriteLine($"視窗位置: ({windowRect.Left}, {windowRect.Top})");
+                                    //Console.WriteLine($"視窗大小: {windowRect.Right - windowRect.Left} x {windowRect.Bottom - windowRect.Top}");
+                                    #endregion
+                                    Do.PackSetForegroundWindow(targetWindowHandle);
+                                    // Action process example:
+                                    Do.SimulateRightMouseClick(targetWindowHandle, Convert.ToInt32(TextBoxDispatcherGetValue(Coordinate_X)), Convert.ToInt32(TextBoxDispatcherGetValue(Coordinate_Y)));
+                                    System.Windows.Forms.SendKeys.SendWait("D:\\oCam");
+                                    //Do.SimulateLeftMouseClick(targetWindowHandle, 899, 156);
+                                    Thread.Sleep(3000);
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"{TextBoxDispatcherGetValue(Window_Name)} Window can't be found.");
+                                }
+                                Thread.Sleep(3000);
+                            }
+                        }, cts.Token);
+                        break;
+                    }
+                case nameof(Stop):
+                    {
+                        cts.Cancel();
+                        break;
+                    }
+                case nameof(Capture_Screen):
+                    {
+                        Do.CaptureScreen(Display_Image);
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+        #region Parameter Screen
+        private void Parameter_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(Save_Config):
+                    {
+                        SaveConfig();
+                        Logger.WriteLog("Save the config.", LogLevel.General, richTextBoxGeneral);
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+        #region Mouse Button Event
+        private List<Ellipse> reddotlist = new List<Ellipse>();
+        private List<System.Drawing.Point> pointlist = new List<System.Drawing.Point>();
+        private void AddPointButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _started = true;
+            _downPoint = e.GetPosition(Display_Image);
+            GetClickPoint();
+        }
+        private void AddPointButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _started = false;
+        }
+        private void DeletePointButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _started = true;
+            RemoveClickPoint();
+        }
+        private void DeletePointButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _started = false;
+        }
+        #endregion
+
+    }
+}
